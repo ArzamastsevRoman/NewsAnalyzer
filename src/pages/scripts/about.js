9759f83e80883c5964ec9d2c62335b5846d97bb6
Roman Arzamastsev <arzamastsevroma@yandex.ru>
2019-11-12 18:18:51 +0300

    update adaptive slider

--- src/pages/scripts/about.js
+++ src/pages/scripts/about.js
@@ -16,37 +16,35 @@ import '../../blocks/glide/glide.core.css'
 
 const glide = new Glide('.glide', {
     type: 'slider',
-    // startAt: 1,
-    // focusAt: 'center',
-    // perView: 3,
-    // gap: 16,
-    // peek: 100,
+    breakpoints: {
+        1440: {
+          type: "slider",
+          perView: 3,
+          startAt: 0,
+          focusAt: 'center',
+          peek: 100,
+          gap: 16,
+        },
+        768: {
+          type: "slider",
+          perView: 2.1,
+          startAt: 0,
+          focusAt: 0,
+          peek: 20,
+          gap: 8
+        },
+        576: {
+          type: "slider",
+          perView: 1.05,
+          startAt: 0,
+          focusAt: 'center',
+          gap: 8,
+          peek: 8
+        }
+    }
 })
 
-if (window.matchMedia('(max-width: 767px)').matches){
-    glide._o.perView = 1.05;
-    glide._o.startAt = 0;
-    glide._o.peek = 8;
-    glide._o.focusAt = 'center';
-    glide._o.gap = 8;
-} else if (window.matchMedia('(max-width: 850px)').matches){
-    glide._o.perView = 2.1;
-    glide._o.startAt = 1;
-    glide._o.peek = 20;
-    glide._o.focusAt = 0;
-    glide._o.gap = 8;
-} else if (window.matchMedia('(max-width: 1440px)').matches) {
-    glide._o.perView = 3;
-    glide._o.startAt = 1;
-    glide._o.peek = 100;
-    glide._o.focusAt = 'center';
-    glide._o.gap = 16;
-} 
-
 glide.mount();
-
-console.log(glide);
-
 
 //подсветка меню
 const about = document.querySelector('#about-link');
