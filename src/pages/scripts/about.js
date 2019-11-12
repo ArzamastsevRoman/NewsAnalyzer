56d45865876a47439b19e2d23425973b4ee8be90
Roman Arzamastsev <arzamastsevroma@yandex.ru>
2019-11-12 16:44:44 +0300

    Adaptive

--- src/pages/scripts/about.js
+++ src/pages/scripts/about.js
@@ -16,14 +16,37 @@ import '../../blocks/glide/glide.core.css'
 
 const glide = new Glide('.glide', {
     type: 'slider',
-    startAt: 1,
-    focusAt: 'center',
-    perView: 3,
-    gap: 16,
-    peek: 100
+    // startAt: 1,
+    // focusAt: 'center',
+    // perView: 3,
+    // gap: 16,
+    // peek: 100,
 })
 
+if (window.matchMedia('(max-width: 767px)').matches){
+    glide._o.perView = 1.05;
+    glide._o.startAt = 0;
+    glide._o.peek = 8;
+    glide._o.focusAt = 'center';
+    glide._o.gap = 8;
+} else if (window.matchMedia('(max-width: 850px)').matches){
+    glide._o.perView = 2.1;
+    glide._o.startAt = 1;
+    glide._o.peek = 20;
+    glide._o.focusAt = 0;
+    glide._o.gap = 8;
+} else if (window.matchMedia('(max-width: 1440px)').matches) {
+    glide._o.perView = 3;
+    glide._o.startAt = 1;
+    glide._o.peek = 100;
+    glide._o.focusAt = 'center';
+    glide._o.gap = 16;
+} 
+
 glide.mount();
+
+console.log(glide);
+
 
 //подсветка меню
 const about = document.querySelector('#about-link');
--- src/pages/scripts/analytics.js
+++ src/pages/scripts/analytics.js
@@ -6,11 +6,3 @@ import '../../blocks/header-black/header-black.css'
 import '../../blocks/news/news.css'
 import '../../blocks/histogram/histogram.css'
 import '../../blocks/footer/footer.css'
-
-const main = document.querySelector('#main-link');
-
-function headerAct () {
-    main.classList.add('header-black__link-act');
-};
-
-headerAct ();
